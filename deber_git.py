d4f5fd718e2412f195f8694734f60222a580b45e
WendyEstefi <wendyestefi1999@gmail.com>
2023-09-22 19:57:44 -0500

    Script en Python

---
+++ deber_git.py
@@ -0,0 +1,10 @@
+#Nombre: Wendy Chicaiza
+# Cargue un paquete de su elección 
+import sklearn as skl
+
+#Dos variables, una que contenga su nombre y la otra su edad
+nombre = 'Wendy'
+edad = 23
+
+# Crear una oración que indique su nombre y su edad utilizando las dos variables.
+print('Mi nombre es',nombre, 'y mi edad es', edad)
